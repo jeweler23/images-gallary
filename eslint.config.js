5def914ceb104abee2bad5b88380084f2a538445
 jeweler23 <lopatin.lopatin2011@gmail.com>
2024-08-21 18:28:54 +0300

    feat:create project

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import antfu from '@antfu/eslint-config';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default antfu({
+  plugins: {
+    '@stylistic': stylistic,
+  },
+  stylistic: {
+    semi: true,
+    quotes: 'single',
+    indent: 'tab',
+    overrides: {
+      'no-return-await': ['error'],
+      'node/prefer-global/process': ['error', 'always'],
+      'style/indent': ['off'], // Сломано в .vue файлах
+    },
+  },
+  typescript: true,
+});
+
+// module.exports = {
+//   root: true,
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:vue/vue3-essential',
+//     '@vue/eslint-config-typescript',
+//     '@antfu',
+//   ],
+//   plugins: [
+//     '@stylistic'
+//   ],
+//   parserOptions: {
+//     ecmaVersion: 'latest',
+//   },
+//   rules: {
+//     '@stylistic/indent': ['error', 2],
+//     '@stylistic/quotes': ['error', 'single'],
+//     '@stylistic/semi': ['error', 'always'],
+//     '@stylistic/max-len': ['error', {
+//       code: 80,
+//       ignoreStrings: true,
+//       ignoreTemplateLiterals: true,
+//     }],
+//   },
+// };
