20baf7554f93087dac9bb2938ffadeec40709569
jeweler23 <lopatin.lopatin2011@gmail.com>
2024-08-25 15:35:19 +0300

    fix: path url base

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/',
+  base: '/images-gallary/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
